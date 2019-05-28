c048c738751d41cf7e0e519e3d549e77d6b7ee0f
MrZhang <332004976@qq.com>
2019-05-28 15:32:02 +0800

    day2

---
+++ vue/day2/3班的课程/2.空间和时间/index.js
@@ -0,0 +1,25 @@
+var a = 1;
+var b = 2;
+// var c;
+
+// 内存
+// c = a;
+// a = b;
+// b = c;
+
+
+// 
+// [a, b] = [b, a]
+
+
+// cpu
+a = a - b; // a = -1 b = 2
+b = a + b; // a = -1 b = 1
+a = b - a;
+
+// a = a ^ b;
+
+var a = $()
+
+a.css();
+a.addClass();
